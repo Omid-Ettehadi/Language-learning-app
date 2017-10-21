67c24b5962445b39d4a2fe6f819abb20aeb796f7
Omid-Ettehadi <omid_ettehadi@yahoo.com>
2017-10-21 12:45:14 -0400

    added button for search of word in Dictionary
    Also commented on the code for sign in page

--- app/src/main/java/com/omidettehadi/language_learning_app/DictionaryFragment.java
+++ app/src/main/java/com/omidettehadi/language_learning_app/DictionaryFragment.java
@@ -8,30 +8,24 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
-
 import javax.net.ssl.HttpsURLConnection;
 
-//add dependencies to your class
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class DictionaryFragment extends Fragment {
 
     private TextView tvResult;
     private EditText etInput;
+    private Button btnSearch;
 
     public DictionaryFragment() {
         // Required empty public constructor
     }
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,17 +34,27 @@ public class DictionaryFragment extends Fragment {
         // Inflate the layout for this fragment
         tvResult = (TextView)view.findViewById(R.id.tvResult);
         etInput = (EditText)view.findViewById(R.id.etInput);
-        new CallbackTask().execute(dictionaryEntries());
+        btnSearch = (Button)view.findViewById(R.id.btnSearch);
+
+        // See if Search Button is pressed
+        // Run Search for the word in the input EditText
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Running lookup from Oxford API dictionary
+                new CallbackTask().execute(dictionaryEntries());
+            }
+        });
         return view;
     }
 
     private String dictionaryEntries() {
+
         final String language = "en";
-        final String word = "Ace"; // etInput.toString();
+        final String word = etInput.getText().toString();
         final String word_id = word.toLowerCase(); //word id is case sensitive and lowercase is required
         return "https://od-api.oxforddictionaries.com:443/api/v1/entries/" + language + "/" + word_id;
     }
-
 
     //in android calling network requests on the main thread forbidden by default
     //create class to do async job
--- app/src/main/java/com/omidettehadi/language_learning_app/SigninActivity.java
+++ app/src/main/java/com/omidettehadi/language_learning_app/SigninActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -20,90 +19,112 @@ import com.google.firebase.auth.FirebaseAuth;
 public class SigninActivity extends AppCompatActivity {
 
     private EditText inputEmail, inputPassword;
+    private Button btnSignup, btnLogin, btnReset;
     private FirebaseAuth auth;
     private ProgressBar progressBar;
-    private Button btnSignup, btnLogin, btnReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Get Firebase auth instance
+        // Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
 
+        // If user is logged in go to Main Activity and skip this page
         if (auth.getCurrentUser() != null) {
-            startActivity(new Intent(SigninActivity.this, MainActivity.class));
+            Intent go_to_MainActivity = new Intent(SigninActivity.this, MainActivity.class);
+            startActivity(go_to_MainActivity);
             finish();
         }
 
-        // set the view now
+        // Set the view now
         setContentView(R.layout.activity_signin);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
-        inputEmail = (EditText) findViewById(R.id.email);
-        inputPassword = (EditText) findViewById(R.id.password);
+        // Definition
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        btnSignup = (Button) findViewById(R.id.btn_signup);
-        btnLogin = (Button) findViewById(R.id.btn_login);
-        btnReset = (Button) findViewById(R.id.btn_reset_password);
+        inputEmail = (EditText) findViewById(R.id.etEmail);
+        inputPassword = (EditText) findViewById(R.id.etPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnReset = (Button) findViewById(R.id.btnResetPassword);
+        btnSignup = (Button) findViewById(R.id.btnSignup);
 
-        btnSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SigninActivity.this, SignupActivity.class));
-            }
-        });
-
-        btnReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SigninActivity.this, ResetPasswordActivity.class));
-            }
-        });
-
+        // Action when Login button is pressed.
+        // Check login info with database.
+        // Go to Navigation Activity if successful
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = inputEmail.getText().toString();
+                final String email = inputEmail.getText().toString();
                 final String password = inputPassword.getText().toString();
 
+                // Check if email address is entered.
                 if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Enter email address!",
+                            Toast.LENGTH_SHORT).show();
                     return;
                 }
-
+                // Check if password is entered.
                 if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Enter password!",
+                            Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 progressBar.setVisibility(View.VISIBLE);
 
-                //authenticate user
+                // Authenticate user's information
                 auth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(SigninActivity.this, new OnCompleteListener<AuthResult>() {
+                        .addOnCompleteListener(SigninActivity.this,
+                                new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                // If sign in fails, display a message to the user. If sign in succeeds
-                                // the auth state listener will be notified and logic to handle the
-                                // signed in user can be handled in the listener.
+                                // If sign in fails, display a message to the user.
+                                // If sign in succeeds, the auth state listener will be notified
+                                // and logic to handle the signed in user can be handled in the
+                                // listener.
                                 progressBar.setVisibility(View.GONE);
                                 if (!task.isSuccessful()) {
-                                    // there was an error
+                                    // Password is short
                                     if (password.length() < 6) {
-                                        inputPassword.setError(getString(R.string.minimum_password));
-                                    } else {
-                                        Toast.makeText(SigninActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                        inputPassword.setError(
+                                                getString(R.string.minimum_password));
                                     }
-                                } else {
-                                    Intent intent = new Intent(SigninActivity.this, MainActivity.class);
-                                    startActivity(intent);
+                                    else {
+                                        Toast.makeText(SigninActivity.this,
+                                                getString(R.string.auth_failed),
+                                                Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                                else {
+                                    Intent go_to_MainActivity = new Intent(SigninActivity.this,
+                                            MainActivity.class);
+                                    startActivity(go_to_MainActivity);
                                     finish();
                                 }
                             }
                         });
+            }
+        });
+
+        // Action when Password Reset button is pressed.
+        // Go to Reset Password Activity.
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go_to_ResetPasswordActivity = new Intent(SigninActivity.this,
+                        ResetPasswordActivity.class);
+                startActivity(go_to_ResetPasswordActivity);
+            }
+        });
+
+        // Action when Sign up button is pressed.
+        // Go to Sign up Activity.
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go_to_SignupActivity = new Intent(SigninActivity.this,
+                        SignupActivity.class);
+                startActivity(go_to_SignupActivity);
             }
         });
     }
