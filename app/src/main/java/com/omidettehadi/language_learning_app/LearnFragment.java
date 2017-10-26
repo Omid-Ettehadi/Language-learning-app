eb8b836713bc1af811fb6254c785aae9ffce45c5
Daniel Charpentier <d.y.charpen@gmail.com>
2017-10-26 13:41:02 -0400

    added audio recording function
    todo: fix permission stuff cause right now it's messy and inelegant, and
    then implement the google API sendoff

--- app/src/main/java/com/omidettehadi/language_learning_app/LearnFragment.java
+++ app/src/main/java/com/omidettehadi/language_learning_app/LearnFragment.java
@@ -1,6 +1,7 @@
 package com.omidettehadi.language_learning_app;
 
 
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import java.io.File;
+import java.io.IOException;
 
 
 /**
@@ -17,20 +19,32 @@ import java.io.File;
  */
 public class LearnFragment extends Fragment {
 
-    MediaRecorder audioCapture = new MediaRecorder();
-    String path = getContext().getCacheDir().getAbsolutePath() + File.separator + "tempAudio";
-    File tempFile = new File(path);
-    Button btnRecord, btnStop, btnPlay;
+    MediaRecorder audioCapture;
+    String path;
+    File tempFile;
+    Button btnRecord, btnStop, btnPlay, btnStopPlay;
+    MediaPlayer playback;
+
+    private boolean permissionToRecordAccepted = false;
+    private boolean permissionToWriteAccepted = false;
+    private String [] permissions = {"android.permission.RECORD_AUDIO", "android.permission.WRITE_EXTERNAL_STORAGE"};
+
     public LearnFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_learn, container, false);
         // Inflate the layout for this fragment
+
+        requestPermissions(permissions, 200);
+
+
+        audioCapture = new MediaRecorder();
+        path = getContext().getCacheDir().getAbsolutePath() + File.separator + "tempAudio.3gp";
+        tempFile = new File(path);
 
         audioCapture.setAudioSource(MediaRecorder.AudioSource.MIC);
         audioCapture.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
@@ -40,6 +54,74 @@ public class LearnFragment extends Fragment {
         btnRecord = (Button)view.findViewById(R.id.btnRecord);
         btnStop = (Button)view.findViewById(R.id.btnStop);
         btnPlay = (Button)view.findViewById(R.id.btnPlay);
+        btnStopPlay = (Button)view.findViewById(R.id.btnStopPlay);
+
+        btnStop.setEnabled(false);
+        btnPlay.setEnabled(false);
+        btnStopPlay.setEnabled(false);
+
+        btnRecord.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                try {
+
+                    audioCapture.prepare();
+                    audioCapture.start();
+                } catch (IllegalStateException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                btnRecord.setEnabled(false);
+                btnStop.setEnabled(true);
+                btnPlay.setEnabled(false);
+                btnStopPlay.setEnabled(false);
+
+
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View view) {
+               audioCapture.stop();
+               audioCapture.reset();
+               audioCapture.setAudioSource(MediaRecorder.AudioSource.MIC);
+               audioCapture.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+               audioCapture.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+               audioCapture.setOutputFile(tempFile);
+               btnRecord.setEnabled(true);
+               btnStop.setEnabled(false);
+               btnPlay.setEnabled(true);
+               btnStopPlay.setEnabled(false);
+           }
+        });
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                btnRecord.setEnabled(false);
+                btnStop.setEnabled(false);
+                btnPlay.setEnabled(false);
+                btnStopPlay.setEnabled(true);
+
+                playback = new MediaPlayer();
+                try {
+                    playback.setDataSource(path);
+                    playback.prepare();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                playback.start();
+            }
+        });
+        btnStopPlay.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View view) {
+               playback.stop();
+               btnRecord.setEnabled(true);
+               btnStop.setEnabled(false);
+               btnPlay.setEnabled(true);
+               btnStopPlay.setEnabled(false);
+           }
+        });
 
         return view;
     }
