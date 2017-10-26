c98a45e12c142226b592dbd58983cb29be88e4f7
Daniel Charpentier <d.y.charpen@gmail.com>
2017-10-26 12:54:57 -0400

    Added basic layout for audio record/api use

--- app/src/main/java/com/omidettehadi/language_learning_app/LearnFragment.java
+++ app/src/main/java/com/omidettehadi/language_learning_app/LearnFragment.java
@@ -1,11 +1,15 @@
 package com.omidettehadi.language_learning_app;
 
 
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.File;
 
 
 /**
@@ -13,7 +17,10 @@ import android.view.ViewGroup;
  */
 public class LearnFragment extends Fragment {
 
-
+    MediaRecorder audioCapture = new MediaRecorder();
+    String path = getContext().getCacheDir().getAbsolutePath() + File.separator + "tempAudio";
+    File tempFile = new File(path);
+    Button btnRecord, btnStop, btnPlay;
     public LearnFragment() {
         // Required empty public constructor
     }
@@ -24,6 +31,16 @@ public class LearnFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_learn, container, false);
         // Inflate the layout for this fragment
+
+        audioCapture.setAudioSource(MediaRecorder.AudioSource.MIC);
+        audioCapture.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        audioCapture.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        audioCapture.setOutputFile(tempFile);
+
+        btnRecord = (Button)view.findViewById(R.id.btnRecord);
+        btnStop = (Button)view.findViewById(R.id.btnStop);
+        btnPlay = (Button)view.findViewById(R.id.btnPlay);
+
         return view;
     }
 
