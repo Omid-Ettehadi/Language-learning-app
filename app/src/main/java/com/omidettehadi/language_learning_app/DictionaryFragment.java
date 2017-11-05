5c86666be7c7f60f0acbad52633a301eb0301b5d
Omid-Ettehadi <omid_ettehadi@yahoo.com>
2017-11-04 23:12:21 -0400

    Pronunciation + Definition
    Implemented google text to speech. Definition is extracted from Oxford Dictionary API

--- app/src/main/java/com/omidettehadi/language_learning_app/DictionaryFragment.java
+++ app/src/main/java/com/omidettehadi/language_learning_app/DictionaryFragment.java
@@ -2,7 +2,10 @@ package com.omidettehadi.language_learning_app;
 
 
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,17 +14,25 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Locale;
+
 import javax.net.ssl.HttpsURLConnection;
 
 
-public class DictionaryFragment extends Fragment {
+public class DictionaryFragment extends Fragment implements TextToSpeech.OnInitListener {
 
     private TextView tvResult;
     private EditText etInput;
-    private Button btnSearch;
+    private Button btnSearch, btnVoice;
+    private TextToSpeech tts;
 
     public DictionaryFragment() {
         // Required empty public constructor
@@ -35,6 +46,8 @@ public class DictionaryFragment extends Fragment {
         tvResult = (TextView)view.findViewById(R.id.tvResult);
         etInput = (EditText)view.findViewById(R.id.etInput);
         btnSearch = (Button)view.findViewById(R.id.btnSearch);
+        btnVoice = (Button)view.findViewById(R.id.btnVoice);
+        tts = new TextToSpeech(getActivity(),DictionaryFragment.this);
 
         // See if Search Button is pressed
         // Run Search for the word in the input EditText
@@ -45,6 +58,17 @@ public class DictionaryFragment extends Fragment {
                 new CallbackTask().execute(dictionaryEntries());
             }
         });
+
+        // See if Voice Button is pressed
+        // Run SpeakOut
+        btnVoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pronounce the word
+                speakOut();
+            }
+        });
+
         return view;
     }
 
@@ -91,11 +115,77 @@ public class DictionaryFragment extends Fragment {
             }
         }
 
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
         @Override
         protected void onPostExecute(String result) {
+            // Definition for local variables
+            String definition = "";
+            int number = 0;
+
             super.onPostExecute(result);
-            tvResult.setText(result);
-            Log.d("This will be my result",result);
+            try {
+                JSONObject results = new JSONObject(result);
+                JSONArray one = results.getJSONArray("results");
+
+                for(int i = 0 ; i < one.length() ; i++){
+                    JSONObject lexicalEntries = one.getJSONObject(i);
+                    JSONArray two = lexicalEntries.getJSONArray("lexicalEntries");
+
+                    for(int j = 0 ; j < two.length() ; j++){
+                        JSONObject entries = two.getJSONObject(j);
+                        JSONArray three = entries.getJSONArray("entries");
+
+                        for(int k = 0 ; k < three.length() ; k++){
+                            JSONObject senses = three.getJSONObject(k);
+                            JSONArray four = senses.getJSONArray("senses");
+
+                            for(int l = 0 ; l < four.length() ; l++) {
+                                JSONObject definitions = four.getJSONObject(l);
+                                JSONArray five = definitions.getJSONArray("definitions");
+                                number += 1 ;
+                                definition += System.lineSeparator();
+                                definition += number + ") " + five.getString(0);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            tvResult.setText(definition);
+
         }
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS)
+        {
+            int result = tts.setLanguage(Locale.US);
+            tts.setPitch(1);
+            tts.setSpeechRate(1);
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "Language is not supported");
+            } else {
+                speakOut();
+            }
+        } else {
+            Log.e("TTS", "Initilization Failed");
+        }
+    }
+    @Override
+    public void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+    private void speakOut() {
+        tts.speak(etInput.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+        Log.e("root ", etInput.getText().toString());
     }
 }
