4b5b8849a5ce97bfcefd22c2265924a1e7b94c86
Daniel Charpentier <d.y.charpen@gmail.com>
2017-11-12 21:59:38 -0500

    Audiorecorder added to learnfragment (non-implemented)

--- app/src/main/java/com/omidettehadi/language_learning_app/LearnFragment.java
+++ app/src/main/java/com/omidettehadi/language_learning_app/LearnFragment.java
@@ -1,6 +1,8 @@
 package com.omidettehadi.language_learning_app;
 
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
@@ -16,6 +18,7 @@ import java.io.IOException;
 public class LearnFragment extends Fragment {
 
     MediaRecorder audioCapture;
+    AudioRecord recorder;
     String path;
     File tempFile;
     Button btnRecord, btnStop, btnPlay, btnStopPlay;
@@ -48,6 +51,16 @@ public class LearnFragment extends Fragment {
         audioCapture.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
         audioCapture.setOutputFile(tempFile);
 
+        recorder = new AudioRecord.Builder()
+                .setAudioSource(MediaRecorder.AudioSource.MIC)
+                .setAudioFormat(new AudioFormat.Builder()
+                        .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                        .setSampleRate(32000)
+                        .setChannelMask(AudioFormat.CHANNEL_IN_MONO)
+                        .build())
+                .setBufferSizeInBytes(AudioRecord.getMinBufferSize(32000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT))
+                .build();
+
         btnStop.setEnabled(false);
         btnPlay.setEnabled(false);
         btnStopPlay.setEnabled(false);
@@ -55,6 +68,7 @@ public class LearnFragment extends Fragment {
         btnRecord.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 try {
+
                     audioCapture.prepare();
                     audioCapture.start();
                 } catch (IllegalStateException e) {
