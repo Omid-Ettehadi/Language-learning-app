048a48df478638862f6f8e1ac91175ce7f1037c4
Omid <omid_ettehadi@yahoo.com>
2017-11-16 22:47:38 -0500

    Problem with FFT
    At then end of the file learn fragment, I'm trying to implement a function to get the fft, but I'm not sure about how

--- app/src/main/java/com/omidettehadi/language_learning_app/LearnFragment.java
+++ app/src/main/java/com/omidettehadi/language_learning_app/LearnFragment.java
@@ -1,8 +1,13 @@
 package com.omidettehadi.language_learning_app;
 
-
-import android.media.MediaPlayer;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.media.MediaRecorder;
+import android.media.audiofx.AcousticEchoCanceler;
+import android.media.audiofx.AutomaticGainControl;
+import android.media.audiofx.NoiseSuppressor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,19 +15,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.omidettehadi.language_learning_app.FFT;
 
 public class LearnFragment extends Fragment {
 
-    MediaRecorder audioCapture;
-    String path;
-    File tempFile;
-    Button btnRecord, btnStop, btnPlay, btnStopPlay;
-    MediaPlayer playback;
+    Button btnRecord, btnStop, btnPlay;
+    File file;
+    AudioRecord AudioRecorded;
+    AudioTrack AudioRecordedTrack;
+    FFT AudioRecordedFFT;
 
-    private boolean permissionToRecordAccepted = false;
-    private boolean permissionToWriteAccepted = false;
+    boolean recording;
+    int sampleFreq = 44100;
 
     public LearnFragment() {
         // Required empty public constructor
@@ -31,86 +47,154 @@ public class LearnFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_learn, container, false);
+
         // Inflate the layout for this fragment
-        final String tempFile;
+        View view = inflater.inflate(R.layout.fragment_learn, container, false);
 
         btnRecord = (Button)view.findViewById(R.id.btnRecord);
         btnStop = (Button)view.findViewById(R.id.btnStop);
-        btnPlay = (Button)view.findViewById(R.id.btnPlay);
-        btnStopPlay = (Button)view.findViewById(R.id.btnStopPlay);
-
-        tempFile = getContext().getCacheDir().getAbsolutePath() + File.separator + "tempAudio.3gp";
-
-        audioCapture = new MediaRecorder();
-        audioCapture.setAudioSource(MediaRecorder.AudioSource.MIC);
-        audioCapture.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        audioCapture.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-        audioCapture.setOutputFile(tempFile);
-
         btnStop.setEnabled(false);
+        btnPlay = (Button)view.findViewById(R.id.btnPlay);
         btnPlay.setEnabled(false);
-        btnStopPlay.setEnabled(false);
+
 
         btnRecord.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                try {
-                    audioCapture.prepare();
-                    audioCapture.start();
-                } catch (IllegalStateException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
                 btnRecord.setEnabled(false);
                 btnStop.setEnabled(true);
                 btnPlay.setEnabled(false);
-                btnStopPlay.setEnabled(false);
+
+                Thread recordThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        recording = true;
+                        StartRecording();
+                    }
+                });
+                recordThread.start();
             }
         });
 
         btnStop.setOnClickListener(new View.OnClickListener() {
            public void onClick(View view) {
-               audioCapture.stop();
-               audioCapture.reset();
-               audioCapture.setAudioSource(MediaRecorder.AudioSource.MIC);
-               audioCapture.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-               audioCapture.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-               audioCapture.setOutputFile(tempFile);
                btnRecord.setEnabled(true);
                btnStop.setEnabled(false);
                btnPlay.setEnabled(true);
-               btnStopPlay.setEnabled(false);
+               recording = false;
            }
         });
 
         btnPlay.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                btnRecord.setEnabled(false);
+                btnRecord.setEnabled(true);
                 btnStop.setEnabled(false);
-                btnPlay.setEnabled(false);
-                btnStopPlay.setEnabled(true);
-                playback = new MediaPlayer();
-                try {
-                    playback.setDataSource(tempFile);
-                    playback.prepare();
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-                playback.start();
+                btnPlay.setEnabled(true);
+                Play();
             }
         });
 
-        btnStopPlay.setOnClickListener(new View.OnClickListener() {
-           public void onClick(View view) {
-               playback.stop();
-               btnRecord.setEnabled(true);
-               btnStop.setEnabled(false);
-               btnPlay.setEnabled(true);
-               btnStopPlay.setEnabled(false);
-           }
-        });
+
         return view;
+    }
+
+    private void StartRecording(){
+
+        file = new File(getContext().getCacheDir().getAbsolutePath() + File.separator , "Recording.pcm");
+
+        try {
+            file.createNewFile();
+
+            OutputStream outputStream = new FileOutputStream(file);
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+            DataOutputStream dataOutputStream = new DataOutputStream(bufferedOutputStream);
+
+            int minBufferSize = AudioRecord.getMinBufferSize(sampleFreq,
+                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT);
+
+            short[] audioData = new short[minBufferSize];
+            AudioRecorded = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                    sampleFreq,
+                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    minBufferSize);
+
+            // Filters for audio
+            AcousticEchoCanceler.create(AudioRecorded.getAudioSessionId());
+            NoiseSuppressor.create(AudioRecorded.getAudioSessionId());
+            AutomaticGainControl.create(AudioRecorded.getAudioSessionId());
+
+            AudioRecorded.startRecording();
+
+            while(recording){
+                int numberOfShort = AudioRecorded.read(audioData, 0, minBufferSize);
+                for(int i = 0; i < numberOfShort; i++){
+                    dataOutputStream.writeShort(audioData[i]);
+                }
+            }
+
+            AudioRecorded.stop();
+            dataOutputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void Play() {
+        file = new File(getContext().getCacheDir().getAbsolutePath() + File.separator , "Recording.pcm");
+
+        int shortSizeInBytes = Short.SIZE/Byte.SIZE;
+        int bufferSizeInBytes = (int)(file.length()/shortSizeInBytes);
+        short[] audioData = new short[bufferSizeInBytes];
+
+        try {
+            InputStream inputStream = new FileInputStream(file);
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+            DataInputStream dataInputStream = new DataInputStream(bufferedInputStream);
+
+            int i = 0;
+            while(dataInputStream.available() > 0){
+                audioData[i] = dataInputStream.readShort();
+                i++;
+            }
+
+            dataInputStream.close();
+
+            AudioRecordedTrack = new AudioTrack(
+                    AudioManager.STREAM_MUSIC,
+                    sampleFreq,
+                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    bufferSizeInBytes,
+                    AudioTrack.MODE_STREAM);
+
+            AudioRecordedTrack.play();
+            AudioRecordedTrack.write(audioData, 0, bufferSizeInBytes);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private FFT getFFT(){
+        file = new File(getContext().getCacheDir().getAbsolutePath() + File.separator , "Recording.pcm");
+
+        int shortSizeInBytes = Short.SIZE/Byte.SIZE;
+        int bufferSizeInBytes = (int)(file.length()/shortSizeInBytes);
+        short[] audioData = new short[bufferSizeInBytes];
+        short[] zeros = new short[bufferSizeInBytes];
+
+        for (int i = 0 ; i < bufferSizeInBytes ; i++){
+            audioData[i] = AudioRecorded.[i];
+            zeros[i] = 0;
+        }
+
+        AudioRecordedFFT.fft(audioData,zeros);
+
+        return AudioRecordedFFT;
     }
 }
