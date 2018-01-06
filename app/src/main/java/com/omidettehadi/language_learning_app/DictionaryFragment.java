7c6f0e999363122a7fa75d80f126221450d1243e
Omid Ettehadi <omid_ettehadi@yahoo.com>
2018-01-06 16:19:47 -0500

    Picture to text and mic to text added

--- app/src/main/java/com/omidettehadi/language_learning_app/DictionaryFragment.java
+++ app/src/main/java/com/omidettehadi/language_learning_app/DictionaryFragment.java
@@ -1,15 +1,21 @@
 package com.omidettehadi.language_learning_app;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -17,23 +23,49 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class DictionaryFragment extends Fragment implements TextToSpeech.OnInitListener {
 
-    private TextView tvSpeakNow;
     private EditText etInput;
-    private Button btnSearch,btnSpeakNow;
+    private Button btnSearch, btnMic, btnCapture, btnCam;
     private SpeechRecognizer speechrecognizer;
     private Intent speechrecognizerIntent;
     private boolean IsListening, packageName;
     private String GoogleScore;
-
+    private SurfaceView cameraView;
+    private CameraSource cameraSource;
+    private final int RequestCameraPermissionID = 1001;
 
     public static String word;
 
     public DictionaryFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case RequestCameraPermissionID: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            break;
+        }
     }
 
     @Override
@@ -43,10 +75,105 @@ public class DictionaryFragment extends Fragment implements TextToSpeech.OnInitL
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dictionary, container, false);
 
-        tvSpeakNow = (TextView) view.findViewById(R.id.tvSpeakNow);
         etInput = (EditText) view.findViewById(R.id.etInput);
         btnSearch = (Button) view.findViewById(R.id.btnSearch);
-        btnSpeakNow = (Button) view.findViewById(R.id.btnSpeakNow);
+        btnMic = (Button) view.findViewById(R.id.btnMic);
+        btnCapture = (Button) view.findViewById(R.id.btnCapture);
+        btnCam = (Button) view.findViewById(R.id.btnCam);
+        cameraView = (SurfaceView) view.findViewById(R.id.surface_view);
+        cameraView.setVisibility(View.INVISIBLE);
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etInput.setVisibility(View.VISIBLE);
+                btnSearch.setVisibility(View.VISIBLE);
+                btnMic.setVisibility(View.VISIBLE);
+                btnCapture.setVisibility(View.INVISIBLE);
+                btnCam.setVisibility(View.VISIBLE);
+                cameraView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btnCam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etInput.setVisibility(View.INVISIBLE);
+                btnSearch.setVisibility(View.INVISIBLE);
+                btnMic.setVisibility(View.INVISIBLE);
+                btnCapture.setVisibility(View.VISIBLE);
+                btnCam.setVisibility(View.INVISIBLE);
+                cameraView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getActivity().getApplicationContext()).build();
+        if (!textRecognizer.isOperational()) {
+
+        } else {
+            cameraSource = new CameraSource.Builder(getActivity().getApplicationContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build();
+            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+                    try {
+                        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                            ActivityCompat.requestPermissions(getActivity(),
+                                    new String[]{android.Manifest.permission.CAMERA},
+                                    RequestCameraPermissionID);
+                            return;
+                        }
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                    cameraSource.stop();
+                }
+            });
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if(items.size() != 0)
+                    {
+                        etInput.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                StringBuilder stringBuilder = new StringBuilder();
+                                for(int i =0;i<items.size();++i)
+                                {
+                                    TextBlock item = items.valueAt(i);
+                                    stringBuilder.append(item.getValue());
+                                    stringBuilder.append("\n");
+                                }
+                                etInput.setText(stringBuilder.toString());
+                            }
+                        });
+                    }
+                }
+            });
+        }
 
         speechrecognizer = SpeechRecognizer.createSpeechRecognizer(getActivity());
         speechrecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
@@ -69,7 +196,7 @@ public class DictionaryFragment extends Fragment implements TextToSpeech.OnInitL
 
         // See if Listen Button is pressed
         // Run Speech Recognition
-        btnSpeakNow.setOnClickListener(new View.OnClickListener() {
+        btnMic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Pronounce the word
@@ -137,7 +264,8 @@ public class DictionaryFragment extends Fragment implements TextToSpeech.OnInitL
         {
             ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
             float[] score = results.getFloatArray(SpeechRecognizer.CONFIDENCE_SCORES);
-
+            etInput.setText(matches.get(0).toString());
+            /*
             for (int i = 0 ; i < matches.size() ; i++){
                 if(matches.get(i).toString().equals(etInput.getText().toString())){
                     GoogleScore = String.valueOf(score[i]*100);
@@ -147,7 +275,7 @@ public class DictionaryFragment extends Fragment implements TextToSpeech.OnInitL
                 else{
                     tvSpeakNow.setText( System.lineSeparator() + "Sorry, What was that?");
                 }
-            }
+            }*/
         }
 
         @Override
