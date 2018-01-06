92e7ce8b243c058fb1ad9d7cb91146e6932d92a2
Omid Ettehadi <omid_ettehadi@yahoo.com>
2018-01-05 20:51:16 -0500

    Updated the UI

--- app/src/main/java/com/omidettehadi/language_learning_app/DictionaryFragment.java
+++ app/src/main/java/com/omidettehadi/language_learning_app/DictionaryFragment.java
@@ -1,8 +1,6 @@
 package com.omidettehadi.language_learning_app;
 
-
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
@@ -11,7 +9,6 @@ import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,33 +17,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Locale;
-
-import javax.net.ssl.HttpsURLConnection;
-
-import static android.content.ContentValues.TAG;
-
 
 public class DictionaryFragment extends Fragment implements TextToSpeech.OnInitListener {
 
-    private TextView tvResult, tvSpeakNow;
+    private TextView tvSpeakNow;
     private EditText etInput;
-    private Button btnSearch, btnVoice, btnSpeakNow;
-    private TextToSpeech tts;
+    private Button btnSearch,btnSpeakNow;
     private SpeechRecognizer speechrecognizer;
     private Intent speechrecognizerIntent;
     private boolean IsListening, packageName;
     private String GoogleScore;
 
+
+    public static String word;
 
     public DictionaryFragment() {
         // Required empty public constructor
@@ -55,16 +39,14 @@ public class DictionaryFragment extends Fragment implements TextToSpeech.OnInitL
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_dictionary, container, false);
+
         // Inflate the layout for this fragment
-        tvResult = (TextView) view.findViewById(R.id.tvResult);
+        View view = inflater.inflate(R.layout.fragment_dictionary, container, false);
+
         tvSpeakNow = (TextView) view.findViewById(R.id.tvSpeakNow);
         etInput = (EditText) view.findViewById(R.id.etInput);
         btnSearch = (Button) view.findViewById(R.id.btnSearch);
-        btnVoice = (Button) view.findViewById(R.id.btnVoice);
         btnSpeakNow = (Button) view.findViewById(R.id.btnSpeakNow);
-        tts = new TextToSpeech(getActivity(), DictionaryFragment.this);
-
 
         speechrecognizer = SpeechRecognizer.createSpeechRecognizer(getActivity());
         speechrecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
@@ -74,24 +56,14 @@ public class DictionaryFragment extends Fragment implements TextToSpeech.OnInitL
         SpeechRecognitionListener listener = new SpeechRecognitionListener();
         speechrecognizer.setRecognitionListener(listener);
 
-
         // See if Search Button is pressed
         // Run Search for the word in the input EditText
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Running lookup from Oxford API dictionary
-                new CallbackTask().execute(dictionaryEntries());
-            }
-        });
-
-        // See if Voice Button is pressed
-        // Run SpeakOut
-        btnVoice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Pronounce the word
-                speakOut();
+                word = etInput.getText().toString();
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.main, new WordProfileFragment()).commit();
             }
         });
 
@@ -107,7 +79,6 @@ public class DictionaryFragment extends Fragment implements TextToSpeech.OnInitL
                 }
             }
         });
-
         return view;
     }
 
@@ -115,9 +86,7 @@ public class DictionaryFragment extends Fragment implements TextToSpeech.OnInitL
         return packageName;
     }
 
-    protected class SpeechRecognitionListener implements RecognitionListener
-    {
-
+    protected class SpeechRecognitionListener implements RecognitionListener {
         @Override
         public void onBeginningOfSpeech()
         {
@@ -187,128 +156,16 @@ public class DictionaryFragment extends Fragment implements TextToSpeech.OnInitL
         }
     }
 
-    private String dictionaryEntries() {
-
-        final String language = "en";
-        final String word = etInput.getText().toString();
-        final String word_id = word.toLowerCase(); //word id is case sensitive and lowercase is required
-        return "https://od-api.oxforddictionaries.com:443/api/v1/entries/" + language + "/" + word_id;
-    }
-
-    //in android calling network requests on the main thread forbidden by default
-    //create class to do async job
-    private class CallbackTask extends AsyncTask<String, Integer, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            //TODO: replace with your own app id and app key
-            final String app_id = "091e7ddf";
-            final String app_key = "af40991963d253e055aaa9cd04a3731d";
-            try {
-                URL url = new URL(params[0]);
-                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
-                urlConnection.setRequestProperty("Accept","application/json");
-                urlConnection.setRequestProperty("app_id",app_id);
-                urlConnection.setRequestProperty("app_key",app_key);
-
-                // read the output from the server
-                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                StringBuilder stringBuilder = new StringBuilder();
-
-                String line = null;
-                while ((line = reader.readLine()) != null) {
-                    stringBuilder.append(line + "\n");
-                }
-
-                return stringBuilder.toString();
-
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-                return e.toString();
-            }
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-        @Override
-        protected void onPostExecute(String result) {
-            // Definition for local variables
-            String definition = "";
-            int number = 0;
-
-            super.onPostExecute(result);
-            try {
-                JSONObject results = new JSONObject(result);
-                JSONArray one = results.getJSONArray("results");
-
-                for(int i = 0 ; i < one.length() ; i++){
-                    JSONObject lexicalEntries = one.getJSONObject(i);
-                    JSONArray two = lexicalEntries.getJSONArray("lexicalEntries");
-
-                    for(int j = 0 ; j < two.length() ; j++){
-                        JSONObject pronunciations = two.getJSONObject(j);
-                        JSONObject entries = two.getJSONObject(j);
-                        JSONArray three = entries.getJSONArray("entries");
-                        JSONArray IPA = pronunciations.getJSONArray("pronunciations");
-
-                        number += 1 ;
-                        definition += System.lineSeparator();
-                        definition += number + ") " + IPA.toString().substring(IPA.toString().lastIndexOf(":")+2,IPA.toString().lastIndexOf('"'));
-
-                        for(int k = 0 ; k < three.length() ; k++){
-                            JSONObject senses = three.getJSONObject(k);
-                            JSONArray four = senses.getJSONArray("senses");
-                            for(int l = 0 ; l < four.length() ; l++) {
-                                JSONObject definitions = four.getJSONObject(l);
-                                JSONArray five = definitions.getJSONArray("definitions");
-                                definition += System.lineSeparator() + " - ";
-                                definition += five.getString(0);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            tvResult.setText(definition);
-
-        }
-    }
-
     @Override
     public void onInit(int status) {
-        if (status == TextToSpeech.SUCCESS)
-        {
-            int result = tts.setLanguage(Locale.US);
-            tts.setPitch(1);
-            tts.setSpeechRate(1);
-            if (result == TextToSpeech.LANG_MISSING_DATA
-                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("TTS", "Language is not supported");
-            } else {
-                speakOut();
-            }
-        } else {
-            Log.e("TTS", "Initilization Failed");
-        }
+
     }
     @Override
     public void onDestroy() {
-        if (tts != null) {
-            tts.stop();
-            tts.shutdown();
-        }
         if (speechrecognizer != null)
         {
             speechrecognizer.destroy();
         }
         super.onDestroy();
-    }
-    private void speakOut() {
-        tts.speak(etInput.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
-        Log.e("root ", etInput.getText().toString());
     }
 }
---
+++ app/src/main/java/com/omidettehadi/language_learning_app/WordProfileFragment.java
@@ -0,0 +1,195 @@
+package com.omidettehadi.language_learning_app;
+
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Locale;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import static com.omidettehadi.language_learning_app.DictionaryFragment.word;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WordProfileFragment extends Fragment implements TextToSpeech.OnInitListener {
+
+    Button btnBack, btnSpeak;
+    TextView tvWord, tvProfile;
+
+    TextToSpeech tts;
+
+    public WordProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_word_profile, container, false);
+
+        btnBack = (Button) view.findViewById(R.id.btnBack);
+        btnSpeak = (Button) view.findViewById(R.id.btnSpeak);
+        tvWord = (TextView) view.findViewById(R.id.tvWord);
+        tvProfile = (TextView) view.findViewById(R.id.tvProfile);
+
+        tts = new TextToSpeech(getActivity(),WordProfileFragment.this);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.main, new DictionaryFragment()).commit();
+            }
+        });
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                speakOut();
+
+            }
+        });
+
+        tvWord.setText(word);
+        new CallbackTask().execute(dictionaryEntries());
+
+        return view;
+    }
+
+    private void speakOut() {
+        tts.speak(word , TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    @Override
+    public void onInit(int i) {
+        if (i == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                btnSpeak.setEnabled(true);
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+
+    }
+
+
+    private String dictionaryEntries() {
+
+        final String language = "en";
+        final String word_id = word.toLowerCase(); //word id is case sensitive and lowercase is required
+        return "https://od-api.oxforddictionaries.com:443/api/v1/entries/" + language + "/" + word_id;
+    }
+
+    //in android calling network requests on the main thread forbidden by default
+    //create class to do async job
+    private class CallbackTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            //TODO: replace with your own app id and app key
+            final String app_id = "091e7ddf";
+            final String app_key = "af40991963d253e055aaa9cd04a3731d";
+            try {
+                URL url = new URL(params[0]);
+                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                urlConnection.setRequestProperty("Accept","application/json");
+                urlConnection.setRequestProperty("app_id",app_id);
+                urlConnection.setRequestProperty("app_key",app_key);
+
+                // read the output from the server
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+
+                return stringBuilder.toString();
+
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                return e.toString();
+            }
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        protected void onPostExecute(String result) {
+            // Definition for local variables
+            String definition = "";
+            int number = 0;
+
+            super.onPostExecute(result);
+            try {
+                JSONObject results = new JSONObject(result);
+                JSONArray one = results.getJSONArray("results");
+
+                for(int i = 0 ; i < one.length() ; i++){
+                    JSONObject lexicalEntries = one.getJSONObject(i);
+                    JSONArray two = lexicalEntries.getJSONArray("lexicalEntries");
+
+                    for(int j = 0 ; j < two.length() ; j++){
+                        JSONObject pronunciations = two.getJSONObject(j);
+                        JSONObject entries = two.getJSONObject(j);
+                        JSONArray three = entries.getJSONArray("entries");
+                        JSONArray IPA = pronunciations.getJSONArray("pronunciations");
+
+                        number += 1 ;
+                        definition += System.lineSeparator();
+                        definition += number + ") " + IPA.toString().substring(IPA.toString().lastIndexOf(":")+2,IPA.toString().lastIndexOf('"'));
+
+                        for(int k = 0 ; k < three.length() ; k++){
+                            JSONObject senses = three.getJSONObject(k);
+                            JSONArray four = senses.getJSONArray("senses");
+                            for(int l = 0 ; l < four.length() ; l++) {
+                                JSONObject definitions = four.getJSONObject(l);
+                                JSONArray five = definitions.getJSONArray("definitions");
+                                definition += System.lineSeparator() + " - ";
+                                definition += five.getString(0);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            tvProfile.setText(definition);
+
+        }
+    }
+
+}
