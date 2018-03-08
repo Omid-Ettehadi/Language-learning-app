454ee18053350de39c015bf86afe7b1c8d9b3e6e
Omid Ettehadi <omid_ettehadi@yahoo.com>
2018-03-08 15:17:57 -0500

    Working on Feedback

--- app/src/main/java/com/omidettehadi/language_learning_app/LearnFragment.java
+++ app/src/main/java/com/omidettehadi/language_learning_app/LearnFragment.java
@@ -93,7 +93,19 @@ public class LearnFragment extends Fragment {
                 btnRecord.setEnabled(true);
                 btnStop.setEnabled(false);
                 btnPlay.setEnabled(true);
+
                 Play();
+
+                InputStream inputStream = null;
+                try {
+                    inputStream = new FileInputStream(file);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                DataInputStream datainputStream = new DataInputStream(inputStream);
+                double[] answer = SampleFFT(datainputStream);
+                String text = answer[0]+ " - " +answer[1]+ " - " + answer[2];
+                textView.setText(text);
             }
         });
 
@@ -146,34 +158,21 @@ public class LearnFragment extends Fragment {
     }
 
     private void Play() {
-        file = new File(getContext().getCacheDir().getAbsolutePath() + File.separator, "Recording.pcm");
 
-        int shortSizeInBytes = Short.SIZE / Byte.SIZE;
-        int bufferSizeInBytes = (int) (file.length() / shortSizeInBytes);
-        short[] audioDataRec = new short[bufferSizeInBytes];
-        double[] audioDataFFT = new double[bufferSizeInBytes];
-        double[] zeros = new double[bufferSizeInBytes];
-        double[] magnitudeFFT = new double[bufferSizeInBytes];
+        int shortSizeInBytes = Short.SIZE/Byte.SIZE;
+
+        int bufferSizeInBytes = (int)(file.length()/shortSizeInBytes);
+        short[] audioData = new short[bufferSizeInBytes];
 
         try {
             InputStream inputStream = new FileInputStream(file);
             BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
             DataInputStream dataInputStream = new DataInputStream(bufferedInputStream);
 
-/*            int i = 0;
-            while (dataInputStream.available() > 0) {
-                audioDataRec[i] = dataInputStream.readShort();
-                i++;
-            }*/
-            double[] freqArray = new double[8192];
-            double[] maxFreq = {0.0, 0.0, 0.0};
             int i = 0;
-            while(dataInputStream.available() > 0) {
-                maxFreq = SampleFFT(dataInputStream);
-                if(i < 8192) {
-                  freqArray[i] = maxFreq[0];
-                  i++;
-                }
+            while(dataInputStream.available() > 0){
+                audioData[i] = dataInputStream.readShort();
+                i++;
             }
 
             dataInputStream.close();
@@ -181,40 +180,13 @@ public class LearnFragment extends Fragment {
             AudioRecordedTrack = new AudioTrack(
                     AudioManager.STREAM_MUSIC,
                     sampleFreq,
-                    AudioFormat.CHANNEL_IN_DEFAULT,
+                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
                     AudioFormat.ENCODING_PCM_16BIT,
                     bufferSizeInBytes,
                     AudioTrack.MODE_STREAM);
 
             AudioRecordedTrack.play();
-            AudioRecordedTrack.write(audioDataRec, 0, bufferSizeInBytes);
-
-
-            /*
-            for (int j = 0; j < bufferSizeInBytes - 1; j++) {
-                audioDataFFT[j] = 100.0;
-                zeros[j] = 100.0;
-            }
-            AudioRecordedFFT = new FFT(bufferSizeInBytes);
-            AudioRecordedFFT.fft(audioDataFFT, zeros);
-
-            // find magnitudes
-            for(int j =0; j < audioDataRec.length ; j++){
-                magnitudeFFT[j] = Math.sqrt(audioDataFFT[j] * audioDataFFT[j] + zeros[j] * zeros[j]);
-            }
-
-            // find largest peak in power spectrum
-            double max_magnitude = magnitudeFFT[0];
-            int max_index = 0;
-            for (int j = 0; j < magnitudeFFT.length; ++j) {
-                if (magnitudeFFT[j] > max_magnitude) {
-                    max_magnitude = (int) magnitudeFFT[j];
-                    max_index = j;
-                }
-            }
-            double freq = max_index * 44100 / bufferSizeInBytes;
-            textView.setText(Double.toString(freq));
-            */
+            AudioRecordedTrack.write(audioData, 0, bufferSizeInBytes);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -286,4 +258,78 @@ public class LearnFragment extends Fragment {
         }
 
     }
+
+/*
+    class IPA {
+        String character;
+        String[] vowel_character;
+        int[] vowel_freq;
+
+    };
+
+    IPA newMem = new IPA();
+
+    private void feedback(double[][] InputFreq){
+        String word = "Hi";
+        String [] IPAs = {"ʌ","ɪ"};
+
+        double[][] LookedUp;
+        for ( int i = 0; i<IPAs.length; i++) {
+            if(IPAs[i] == "iː") {
+                final static String[] vowel_character = new String[]{"iː", "Close", "Front", "Long"};
+                static int[] vowel_1_freq = new int[]{280, 2620, 3380};
+            }
+            if (IPAs[i] == "ɪ") {
+                final static String[] vowel_2_character = new String[]{"ɪ", "Close", "Front", "Short"};
+                static int[] vowel_2_freq = new int[]{360, 2220, 2960};
+            }
+            if (IPAs[i] == "e") {
+                public final static String[] vowel_3_character = new String[]{"e", "Half-open", "Front", "Short"};
+                public static int[] vowel_3_freq = new int[]{600, 2060, 2840};
+            }
+            if (IPAs[i] == "æ") {
+                public final static String[] vowel_4_character = new String[]{"æ", "Open", "Front", "Short"};
+                public static int[] vowel_4_freq = new int[]{800, 1760, 2500};
+            }
+            if (IPAs[i] == "ʌ") {
+                public final static String[] vowel_5_character = new String[]{"ʌ", "Open", "Central", "Short"};
+                public static int[] vowel_5_freq = new int[]{760, 1320, 2500};
+            }
+            if (IPAs[i] == "ɑː") {
+                public final static String[] vowel_6_character = new String[]{"ɑː", "Open", "Back", "Long"};
+                public static int[] vowel_6_freq = new int[]{740, 1180, 2640};
+            }
+            if (IPAs[i] == "ɒ") {
+                public final static String[] vowel_7_character = new String[]{"ɒ", "Open", "Back", "Short"};
+                public static int[] vowel_7_freq = new int[]{560, 920, 2560};
+            }
+            if (IPAs[i] == "ɔː") {
+                public final static String[] vowel_8_character = new String[]{"ɔː", "Half-open", "Back", "Long"};
+                public static int[] vowel_8_freq = new int[]{480, 760, 2620};
+            }
+            if (IPAs[i] == "ʊ") {
+                public final static String[] vowel_9_character = new String[]{"ʊ", "Close", "Back", "Short"};
+                public static int[] vowel_9_freq = new int[]{380, 940, 2300};
+            }
+            if (IPAs[i] == "uː") {
+                public final static String[] vowel_10_character = new String[]{"uː", "Close", "Back", "Long"};
+                public static int[] vowel_10_freq = new int[]{320, 920, 2200};
+            }
+            if (IPAs[i] == "ɜː") {
+                public final static String[] vowel_11_character = new String[]{"ɜː", "Half-open", "Central", "Long"};
+                public static int[] vowel_11_freq = new int[]{560, 1480, 2520};
+            }
+            if (IPAs[i] == "ə") {
+                public final static String[] vowel_12_character = new String[]{"ə", "Half-open", "Central", "Short"};
+                public static int[] vowel_12_freq = new int[]{500, 1500, 3000};
+            }
+        }
+
+        for(int i = 0; i<InputFreq.length; i++){
+            if(InputFreq[i][0] == LookedUp[i)
+        }
+
+
+
+    }*/
 }
